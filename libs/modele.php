4eb1e30fa1388f65116f980ac84ffd154e0042f9
theogllrd <theogllrd@gmail.com>
2018-11-19 08:27:27 +0100

    modele

--- libs/modele.php
+++ libs/modele.php
@@ -5,61 +5,42 @@
 include_once("maLibSQL.pdo.php");
 
 
-function listerUtilisateurs($classe = "both")
+
+
+
+
+
+
+function recupGeneriqueBdd($table,$colonne,$condition="")
 {
-	// NB : la présence du symbole '=' indique la valeur par défaut du paramètre s'il n'est pas fourni
-	// Cette fonction liste les utilisateurs de la base de données 
-	// et renvoie un tableau d'enregistrements. 
-	// Chaque enregistrement est un tableau associatif contenant les champs 
-	// id,pseudo,blacklist,connecte,couleur
-
-	// Lorsque la variable $classe vaut "both", elle renvoie tous les utilisateurs
-	// Lorsqu'elle vaut "bl", elle ne renvoie que les utilisateurs blacklistés
-	// Lorsqu'elle vaut "nbl", elle ne renvoie que les utilisateurs non blacklistés
-
-	$SQL = "select * from users";
-	if ($classe == "bl")
-		$SQL .= " where blacklist=1";
-	if ($classe == "nbl")
-		$SQL .= " where blacklist=0";
-	
-	// echo $SQL;
-	return parcoursRs(SQLSelect($SQL));
-
-}
-
-
-function interdireUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "blacklist" à vrai
-	$SQL = "UPDATE users SET blacklist=1 WHERE id='$idUser'";
-	// les apostrophes font partie de la sécurité !! 
-	// Il faut utiliser addslashes lors de la récupération 
-	// des données depuis les formulaires
-
-	SQLUpdate($SQL);
-}
-
-function autoriserUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "blacklist" à faux 
-	$SQL = "UPDATE users SET blacklist=0 WHERE id='$idUser'";
-	SQLUpdate($SQL);
-}
-
-function verifUserBdd($login,$passe)
-{
-	// Vérifie l'identité d'un utilisateur 
-	// dont les identifiants sont passes en paramètre
-	// renvoie faux si user inconnu
-	// renvoie l'id de l'utilisateur si succès
-
-	$SQL="SELECT id FROM users WHERE pseudo='$login' AND passe='$passe'";
-
+	$SQL="SELECT $colonne FROM $table $condition";
 	return SQLGetChamp($SQL);
-	// si on avait besoin de plus d'un champ
-	// on aurait du utiliser SQLSelect
 }
+
+function recupGeneriqueBddFE($table,$colonne,$condition="")
+{
+	$SQL="SELECT $colonne FROM $table $condition";
+	return SQLSelect($SQL);
+}
+
+function updateGeneriqueBdd($table,$colonne,$valeur,$condition="")
+{
+	$SQL = "UPDATE $table SET $colonne='$valeur' $condition";
+	return SQLGetChamp($SQL);
+}
+
+function deleteGeneriqueBdd($table,$condition)
+{
+	$SQL = "DELETE FROM $table $condition";
+	return SQLGetChamp($SQL);
+}
+
+function insertGeneriqueBdd($table,$colonnes,$valeurs)
+{
+	$SQL = "INSERT INTO $table($colonnes) VALUES($valeurs)";
+	return SQLInsert($SQL);	
+}
+
 
 
 ?>
