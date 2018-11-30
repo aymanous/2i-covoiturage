dc211f0cc535dbef0bf396e70d51fabe56265ecb
Leandree <carpentier.leandre@gmail.com>
2018-11-30 16:35:15 +0100

    delete trajet

--- js/script.js
+++ js/script.js
@@ -33,6 +33,21 @@ $(document).ready(function(){
     })
 
 
+    $(".deleteTrajet").on("click", function(){
+
+        console.log($(this).attr("name"));
+        // CASE OU ON JOIN UN TRAJET
+        // Comment traduire une variable js en variable php
+        //<?php insertGeneriqueBdd("utilisateurtrajet","idUtilisateur,idTrajet","15,15"); ?>
+
+        $.ajax({  url:"controleur.php",
+            data: {"action":"deleteTrajet",
+                "idTrajet" : $(this).attr("name")}
+        });
+      
+    })
+
+
 
 
 
